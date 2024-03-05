a46e77f6bdcf51b854285a25b699c08887cedc53
ankurshashwat <1nkurshashwat@gmail.com>
2024-03-05 23:49:15 +0530

    refactored application layout & implemented global styles

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+// import { Toaster } from "@/components/ui/toaster";
+// import Navbar from "@/components/shared/navbar/Navbar";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="background-light850_dark100 relative">
+      {/* <Navbar /> */}
+      <div className="flex">
+        <div className="mx-auto w-full max-w-5xl">{children}</div>
+      </div>
+      {/* <Toaster /> */}
+    </main>
+  );
+};
+
+export default Layout;
