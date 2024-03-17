e080b527f6aa2f26720d661eac3a802204928e1d
ankurshashwat <1nkurshashwat@gmail.com>
2024-03-17 22:03:55 +0530

    update public routes

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
  
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/', '/api/webhook/clerk'],
+  publicRoutes: ['/', '/api/webhook'],
   // Routes that can always be accessed, and have
   // no authentication information
-  ignoredRoutes: ['/api/webhook/clerk'],
+  ignoredRoutes: ['/api/webhook'],
 });
  
 export const config = {
