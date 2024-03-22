ae4463d1a969de6ab66470fcdd700ae394b3c437
ankurshashwat <1nkurshashwat@gmail.com>
2024-03-23 00:09:32 +0530

    updated map props

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { useState, useRef, useEffect, useCallback } from "react";
 import {
@@ -12,7 +12,11 @@ import { Input } from "./ui/input";
 import Image from "next/image";
 
 export interface MapProps {
-  updateAddress: (location: { address: string; lat: number; lng: number }) => void;
+  updateAddress: (location: {
+    address: string;
+    lat: number;
+    lng: number;
+  }) => void;
 }
 
 const containerStyle = {
@@ -24,9 +28,11 @@ const containerStyle = {
 const libraries: Libraries = ["places"];
 
 const Map = ({ updateAddress }: MapProps) => {
-  const [marker, setMarker] = useState<{ address: string; lat: number; lng: number } | null>(
-    null
-  );
+  const [marker, setMarker] = useState<{
+    address: string;
+    lat: number;
+    lng: number;
+  } | null>(null);
   // eslint-disable-next-line no-undef
   const searchBoxRef = useRef<google.maps.places.Autocomplete | null>(null);
   // eslint-disable-next-line no-undef
@@ -37,14 +43,15 @@ const Map = ({ updateAddress }: MapProps) => {
     libraries,
   });
 
-  const onMapLoad = useCallback((map : any) => {
+  // eslint-disable-next-line no-undef
+  const onMapLoad = useCallback((map: google.maps.Map) => {
     mapRef.current = map;
     const input = document.getElementById("search-box") as HTMLInputElement;
     searchBoxRef.current = new window.google.maps.places.Autocomplete(input, {
-      componentRestrictions: { country: "IN" }, 
+      componentRestrictions: { country: "IN" },
     });
     searchBoxRef.current.addListener("place_changed", onSearchBoxPlacesChanged);
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const onSearchBoxPlacesChanged = useCallback(() => {
@@ -72,14 +79,18 @@ const Map = ({ updateAddress }: MapProps) => {
     }
   }, []);
 
-  const onMapClick = (event : any) => {
-    const newMarker = {
-      address: event.latLng.toString(),
-      lat: event.latLng.lat(),
-      lng: event.latLng.lng(),
-    };
+  // eslint-disable-next-line no-undef
+  const onMapClick = (event: any) => {
+    // Check if event.latLng is not null
+    if (event.latLng) {
+      const newMarker = {
+        address: event.latLng.toString(),
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      };
 
-    setMarker(newMarker);
+      setMarker(newMarker);
+    }
   };
 
   useEffect(() => {
@@ -96,15 +107,28 @@ const Map = ({ updateAddress }: MapProps) => {
     <div style={{ width: "100%", height: "100%" }}>
       {isLoaded && (
         <>
-          <div className={`background-light800_darkgradient mb-4 flex size-full min-h-[56px] grow items-center gap-4 rounded-[10px] px-4`}>
-            <Image src="assets/icons/search.svg" alt="search icon" width={24} height={24} className="cursor-pointer" />
-            <Input id="search-box" type="text" placeholder="Search for your location" className="text-dark400_light700 paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none" />
+          <div
+            className={`background-light800_darkgradient mb-4 flex size-full min-h-[56px] grow items-center gap-4 rounded-[10px] px-4`}
+          >
+            <Image
+              src="assets/icons/search.svg"
+              alt="search icon"
+              width={24}
+              height={24}
+              className="cursor-pointer"
+            />
+            <Input
+              id="search-box"
+              type="text"
+              placeholder="Search for your location"
+              className="text-dark400_light700 paragraph-regular no-focus placeholder text-dark400_light700 border-none bg-transparent shadow-none outline-none"
+            />
           </div>
           <div style={{ flex: 1, marginBottom: 16 }}>
             <GoogleMap
               mapContainerStyle={containerStyle}
-              center={marker || { lat: 25.3176, lng: 82.9739 }} 
-              zoom={12} 
+              center={marker || { lat: 25.3176, lng: 82.9739 }}
+              zoom={12}
               onLoad={onMapLoad}
               onClick={onMapClick}
               options={{ styles: darkMapStyle }}
